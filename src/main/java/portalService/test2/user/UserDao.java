630af3f4225dfd75396d1783ce0a8f204fb75fb7
endyd1567 <endyd1567@gmail.com>
2023-04-21 08:26:30 +0900

    0. 연결 설정

--- src/main/java/portalService/test2/user/UserDao.java
+++ src/main/java/portalService/test2/user/UserDao.java
@@ -1,2 +1,27 @@
-package portalService.test2.user;public class UserDao {
+package portalService.test2.user;
+
+import java.sql.*;
+
+import static portalService.test2.connection.ConnectionConst.*;
+
+public class UserDao {
+    public User findById(Long id) throws SQLException {
+        Connection con = DriverManager.getConnection(URL_JEJU, USERNAME_JEJU, PASSWORD_JEJU);
+        String sql = "select id,name,password from userinfo where id=?";
+        PreparedStatement psmt = con.prepareStatement(sql);
+        psmt.setLong(1, id);
+        ResultSet rs = psmt.executeQuery();
+
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        rs.close();
+        psmt.close();
+        con.close();
+
+        return user;
+    }
 }
--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package portalService.test2.user;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class UserDaoTest {
-  
+
+    @Test
+    public void get() throws SQLException, SQLException {
+
+        Long id = 1l;
+        String name = "umdu";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.findById(1l);
+
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPassword()).isEqualTo(password);
+
+
+    }
+
 }
