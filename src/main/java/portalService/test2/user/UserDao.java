73f9a7b13ba54e89b9fdfd68d7cfbf23bfa08c63
endyd1567 <endyd1567@gmail.com>
2023-04-21 08:50:55 +0900

    7. 장애 해결 : 리소스 해제

--- src/main/java/portalService/test2/user/UserDao.java
+++ src/main/java/portalService/test2/user/UserDao.java
@@ -16,40 +16,78 @@ public class UserDao {
     }
 
     public User findById(Long id) throws SQLException {
-        Connection con = dataSource.getConnection();
-        String sql = "select id,name,password from userinfo where id=?";
-        PreparedStatement psmt = con.prepareStatement(sql);
-        psmt.setLong(1, id);
-        ResultSet rs = psmt.executeQuery();
 
-        rs.next();
+        Connection con = null;
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+        User user;
 
-        User user = new User();
-        user.setId(rs.getLong("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-        rs.close();
-        psmt.close();
-        con.close();
+        try {
+            con = dataSource.getConnection();
+            String sql = "select id,name,password from userinfo where id=?";
+            psmt = con.prepareStatement(sql);
+            psmt.setLong(1, id);
+            rs = psmt.executeQuery();
 
+            rs.next();
+
+            user = new User();
+            user.setId(rs.getLong("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                psmt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return user;
     }
 
     public void insert(User user) throws SQLException {
-        Connection con = dataSource.getConnection();
-        String sql = "insert into userinfo(name,password) values(?,?) ";
-        PreparedStatement psmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-        psmt.setString(1, user.getName());
-        psmt.setString(2, user.getPassword());
-        psmt.executeUpdate();
 
-        ResultSet rs = psmt.getGeneratedKeys();
-        rs.next();
-        user.setId(rs.getLong(1));
+        Connection con = null;
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
 
-        rs.close();
-        psmt.close();
-        con.close();
+        try {
+            con = dataSource.getConnection();
+            String sql = "insert into userinfo(name,password) values(?,?) ";
+            psmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            psmt.setString(1, user.getName());
+            psmt.setString(2, user.getPassword());
+            psmt.executeUpdate();
 
+            rs = psmt.getGeneratedKeys();
+            rs.next();
+            user.setId(rs.getLong(1));
+        }finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                psmt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
