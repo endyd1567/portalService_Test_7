56b6a1c7ad02254b51d2b460ca16af442d33391b
endyd1567 <endyd1567@gmail.com>
2023-04-21 09:12:07 +0900

    11. template callback pattern

--- src/main/java/portalService/test2/user/UserDao.java
+++ src/main/java/portalService/test2/user/UserDao.java
@@ -17,23 +17,46 @@ public class UserDao {
     }
 
     public User findById(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new FindStatementStrategy(id);
+        StatementStrategy statementStrategy = con -> {
+            String sql = "select id,name,password from userinfo where id=?";
+            PreparedStatement psmt = con.prepareStatement(sql);
+            psmt.setLong(1, id);
+            return psmt;
+        };
         User user = jdbcContext.jdbcContextForFind(statementStrategy);
         return user;
     }
 
     public void insert(User user) throws SQLException {
-        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
+        StatementStrategy statementStrategy = con -> {
+            String sql = "insert into userinfo(name,password) values(?,?) ";
+            PreparedStatement psmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            psmt.setString(1, user.getName());
+            psmt.setString(2, user.getPassword());
+            return psmt;
+        };
         jdbcContext.jdbcContextForInsert(user,statementStrategy);
     }
 
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        StatementStrategy statementStrategy = con -> {
+            String sql = "update userinfo set name=? , password=? where id=? ";
+            PreparedStatement psmt = con.prepareStatement(sql);
+            psmt.setString(1, user.getName());
+            psmt.setString(2, user.getPassword());
+            psmt.setLong(3,user.getId());
+            return psmt;
+        };
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        StatementStrategy statementStrategy = con -> {
+            String sql = "delete from userinfo where id=? ";
+            PreparedStatement psmt = con.prepareStatement(sql);
+            psmt.setLong(1, id);
+            return psmt;
+        };
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 }
